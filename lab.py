032dfc23d1edd88ac8be4cde68db3f040fa93e9f
rach <rachell.250006@gmail.com>
2018-11-04 01:44:56 -0300

    :x

--- lab.py
+++ lab.py
@@ -1,8 +1,7 @@
 from input import input as input_flechas
 import os
-import time
 import random
-
+import json
 def intro_al_juego():
     print("UN POLLITO HA PERDIDO A SU MADRE")
     print("🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣🐣")
@@ -19,16 +18,67 @@ def nivel():
         sub = []
     lab[0][0] = 2
     lab[14][14] = 3
-    lab[0][1] = "-"
-    #lab[1][1] = "-"
-    #lab[1][2] = "-"
-    #lab[2][2] = "-"
-    #lab[3][1] = "-"
-    #lab[3][2] = "-"
-    #lab[3][3] = "-"
-    lab[3][4] = "-"
+    #coordenadas que indican la salida estandar del juego 
+    lab[0][1] = 0
+    lab[1][1] = 0
+    lab[2][2] = 0
+    lab[3][1] = 0
+    lab[3][2] = 0
+    lab[3][3] = 0
+    lab[3][4] = 0
+    lab[4][6] = 0
+    lab[5][6] = 0
+    lab[6][6] = 0
+    lab[6][7] = 0
+    lab[6][8] = 0
+    lab[7][8] = 0
+    lab[8][8] = 0
+    lab[8][9] = 0
+    lab[8][10] = 0
+    lab[9][10] = 0
+    lab[10][10] = 0
+    lab[11][10] = 0
+    lab[12][10] = 0
+    lab[13][10] = 0
+    lab[13][11] = 0
+    lab[13][12] = 0
+    lab[13][13] = 0
+    lab[13][14] = 0
+    lab[14][14] = 0
     return lab
 
+def lento(lab):
+    x = 0
+    y = 0
+    for linea in lab:
+        #idea: ir creando movimientos aleatorios e ir guardandolos en archivos json y luego ejecutar los movimientos
+
+
+
+def menu_psolucion(lab):
+    valido = True
+    while valido:
+        print("""\n Seleccione la opción que desee realizar:
+        1.- seguir jugando
+        2.- encontrar la salida más rápida
+        3.- ir por el camino más lento
+        4.- ir por el camino estándar""")
+        opcion = input()
+        #si la opcion es 1 vuelve a llamar a la funcion del juego para que vuelva a jugar. 
+        if opcion == "1":
+            juego()
+        
+        elif opcion == "3":
+                lento()
+
+        else:
+            print("esa opción no es válida, intentelo de nuevo")
+            valido = False
+
+    #el siguiente bloque hace que si la opcion que es ingresada no está en el menu, este
+    #se vuelva a mostrar
+    if valido is False:
+        menu_psolucion()
 def dibujar(lab):
     lab = lab
     x = 0
@@ -47,7 +97,7 @@ def dibujar(lab):
                 print("🐔")
                 x = x + 1
             elif item == 0:
-                print("  ", end='')
+                print(" ", end='')
                 x = 0
                 y = y + 1
             elif item == "-":
@@ -64,7 +114,7 @@ def juego(lab):
     lab = lab
 
     while True:
-        dibujar(lab)
+        dibujar()
         tecla = input_flechas()
         if tecla.name == "arrow up":
             # si el cuadradito de arriba NO es una pared
@@ -85,7 +135,9 @@ def juego(lab):
             # si el cuadradito a la izquierda NO es una pared
             if lab(pos_jugador_x - 1, pos_jugador_y) != "x":
                     pos_jugador_x = pos_jugador_x - 1
-
+        #si la posicion es mayor o igual a 7,7 llama al menu
+        elif pos_jugador_x >= 7 and pos_jugador_y >= 7:
+            menu_psolucion()
         if pos_jugador_x == meta_x and pos_jugador_y == meta_y:
             os.system("clear")
             print("¡El pollito está con su mamá!")
@@ -96,3 +148,5 @@ intro_al_juego()
 lab = nivel()
 dibujar(lab)
 juego(lab)
+lento(lab)
+menu_psolucion(lab)
